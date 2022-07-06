44e7751f7362c53a4f12c170a7cae52f388cbc48
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-06 22:41:06 +0200

    feat: udpate DumpsterManager with getById and create implementations

--- src/main/kotlin/swc/controllers/DumpsterManager.kt
+++ src/main/kotlin/swc/controllers/DumpsterManager.kt
@@ -1,22 +1,32 @@
 package swc.controllers
 
+import com.azure.digitaltwins.core.implementation.models.ErrorResponseException
+import swc.adapters.DumpsterDeserialization
+import swc.adapters.DumpsterDeserialization.toDumpster
+import swc.adapters.DumpsterSerialization.toJson
 import swc.controllers.errors.DumpsterNotFoundException
 import swc.entities.Dumpster
-import swc.entities.WasteName
 
 object DumpsterManager : Manager {
 
     override fun getDumpsters(): List<Dumpster> {
         TODO("Not yet implemented")
     }
-
+    @Throws(NoSuchElementException::class)
     override fun getDumpsterById(id: String): Dumpster {
         val query = "SELECT * FROM digitaltwins WHERE ${'$'}dtId='$id' "
+        val response: String
         try {
-            AzureAuthentication.authClient.query(query, String::class.java).first()
-        } catch (e: NoSuchElementException) {
-            throw DumpsterNotFoundException("Dumpster with id $id not found")
+            response = AzureAuthentication.authClient.getDigitalTwin(id, String::class.java)
+        } catch (e: ErrorResponseException) {
+            when (e.response.statusCode) {
+                404 -> throw DumpsterNotFoundException("Dumpster with id $id not found")
+                else -> error { e.printStackTrace() }
+            }
         }
-        return Dumpster.from(1500.0, WasteName.GLASS)
+        return DumpsterDeserialization.parse(response).toDumpster()
     }
+
+    override fun createDumpster(dumpster: Dumpster): String = AzureAuthentication.authClient
+        .createOrReplaceDigitalTwin(dumpster.id, dumpster.toJson().toString(), String::class.java)
 }
