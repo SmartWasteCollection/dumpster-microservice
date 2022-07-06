de6885ce6cfb4a06709f82680870a320bafc016b
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-06 22:41:06 +0200

    feat: udpate DumpsterManager with getById and create implementations

--- src/main/kotlin/swc/controllers/Manager.kt
+++ src/main/kotlin/swc/controllers/Manager.kt
@@ -4,6 +4,6 @@ import swc.entities.Dumpster
 
 interface Manager {
     fun getDumpsters(): List<Dumpster>
-
     fun getDumpsterById(id: String): Dumpster
+    fun createDumpster(dumpster: Dumpster): String
 }
