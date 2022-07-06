d7726a99d0748beaa591f465fc106c99fa141594
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-07 00:38:55 +0200

    feat: add GetDumpstersUseCase implementation and tests

--- src/main/kotlin/swc/usecases/GetDumpstersUseCase.kt
+++ src/main/kotlin/swc/usecases/GetDumpstersUseCase.kt
@@ -1,11 +1,9 @@
 package swc.usecases
 
+import swc.controllers.DumpsterManager
 import swc.entities.Dumpster
-import swc.entities.WasteName
 
 class GetDumpstersUseCase : UseCase<List<Dumpster>> {
-    override fun execute(): List<Dumpster> = listOf(
-        Dumpster.from(1234.0, WasteName.ORGANIC),
-        Dumpster.from(1234.0, WasteName.GLASS),
-    )
+    override fun execute(): List<Dumpster> =
+        DumpsterManager.getDumpsters()
 }
--- src/test/kotlin/UseCasesTest.kt
+++ src/test/kotlin/UseCasesTest.kt
@@ -1,14 +1,18 @@
+
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import swc.adapters.DumpsterDeserialization
 import swc.adapters.DumpsterDeserialization.toDumpster
+import swc.controllers.AzureAuthentication
+import swc.controllers.AzureQueries
 import swc.controllers.errors.DumpsterNotFoundException
 import swc.entities.Dumpster
 import swc.entities.Volume
 import swc.entities.WasteName
 import swc.usecases.CreateDumpsterUseCase
 import swc.usecases.GetDumpsterByIdUseCase
+import swc.usecases.GetDumpstersUseCase
 import swc.usecases.OpenDumpsterUseCase
 
 class UseCasesTest : DescribeSpec({
@@ -52,6 +56,18 @@ class UseCasesTest : DescribeSpec({
             OpenDumpsterUseCase(dumpster.id).execute()
             val remoteDumpster = GetDumpsterByIdUseCase(dumpster.id).execute()
             remoteDumpster.isOpen shouldBe false
+        }
+    }
+
+    describe("GetDumpstersUseCase") {
+        it("should return the same number of instances in Azure Platform") {
+            val dumpsters = GetDumpstersUseCase().execute()
+            println()
+            dumpsters.forEach { println(it) }
+            println()
+            val count = AzureAuthentication.authClient.query(AzureQueries.GET_DUMPSTERS_COUNT, String::class.java).first()
+
+            dumpsters.size shouldBe DumpsterDeserialization.parse(count)["COUNT"].asInt
         }
     }
 })
