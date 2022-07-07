8cadddfd25f113d67096388ae467baa91574de72
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-07 23:24:06 +0200

    feat: add support for Dumpster's relationship with CollectionPoints

--- src/main/kotlin/swc/usecases/dumpster/CreateDumpsterUseCase.kt
+++ src/main/kotlin/swc/usecases/dumpster/CreateDumpsterUseCase.kt
@@ -1,8 +1,12 @@
 package swc.usecases.dumpster
 
+import swc.entities.CollectionPoint
 import swc.entities.Dumpster
 import swc.usecases.UseCase
 
-class CreateDumpsterUseCase(private val dumpster: Dumpster) : UseCase<Dumpster> {
-    override fun execute(): Dumpster = manager().createDumpster(dumpster)
+class CreateDumpsterUseCase(
+    private val dumpster: Dumpster,
+    private val collectionPoint: CollectionPoint,
+) : UseCase<Dumpster> {
+    override fun execute(): Dumpster = manager().createDumpster(dumpster, collectionPoint)
 }
