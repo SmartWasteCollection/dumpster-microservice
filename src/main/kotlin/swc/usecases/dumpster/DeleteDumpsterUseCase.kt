1a765d41f4ebd477af7904f97f2ba6bac3db5320
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-07 23:28:53 +0200

    chore: add return type to delete usecases

--- src/main/kotlin/swc/usecases/dumpster/DeleteDumpsterUseCase.kt
+++ src/main/kotlin/swc/usecases/dumpster/DeleteDumpsterUseCase.kt
@@ -1,7 +1,12 @@
 package swc.usecases.dumpster
 
+import swc.entities.Dumpster
 import swc.usecases.UseCase
 
-class DeleteDumpsterUseCase(private val id: String) : UseCase<Unit> {
-    override fun execute() = manager().deleteDumpster(id)
+class DeleteDumpsterUseCase(private val id: String) : UseCase<Dumpster> {
+    override fun execute(): Dumpster {
+        val oldDumpster = manager().getDumpsterById(id)
+        manager().deleteDumpster(id)
+        return oldDumpster
+    }
 }
