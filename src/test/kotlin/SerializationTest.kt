8871b8d2e5da639f29797e9d9689eec62c2636d4
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-07 18:30:36 +0200

    feat: add Serialization tests

--- src/test/kotlin/SerializationTest.kt
+++ src/test/kotlin/SerializationTest.kt
@@ -2,9 +2,13 @@ import com.google.gson.JsonObject
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
+import swc.adapters.CollectionPointsDeserialization.toCollectionPoint
+import swc.adapters.CollectionPointsSerialization.toJson
 import swc.adapters.DumpsterDeserialization.toDumpster
 import swc.adapters.DumpsterSerialization.toJson
+import swc.entities.CollectionPoint
 import swc.entities.Dumpster
+import swc.entities.Position
 import swc.entities.WasteName
 
 class SerializationTest : DescribeSpec({
@@ -22,6 +26,20 @@ class SerializationTest : DescribeSpec({
             typeAfter.isOpen shouldBe typeBefore.isOpen
             typeAfter.isWorking shouldBe typeBefore.isWorking
             typeAfter.occupiedVolume shouldBe typeBefore.occupiedVolume
+        }
+    }
+
+    describe("A CollectionPoint") {
+        it("should be serialized and deserialized correctly") {
+            val typeBefore = CollectionPoint(position = Position(0L, 0L))
+
+            val typeSerialized = typeBefore.toJson()
+            typeSerialized.shouldBeInstanceOf<JsonObject>()
+
+            val typeAfter = typeSerialized.toCollectionPoint()
+            typeAfter.shouldBeInstanceOf<CollectionPoint>()
+            typeAfter.id shouldBe typeBefore.id
+            typeAfter.position shouldBe typeBefore.position
         }
     }
 })
