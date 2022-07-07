5adf58e0e723ff22a5102684e4bfe7b441f24d9b
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-07 15:01:16 +0200

    fix: refactor of update functions return types

--- src/main/kotlin/swc/usecases/CreateDumpsterUseCase.kt
+++ src/main/kotlin/swc/usecases/CreateDumpsterUseCase.kt
@@ -2,7 +2,6 @@ package swc.usecases
 
 import swc.entities.Dumpster
 
-class CreateDumpsterUseCase(private val dumpster: Dumpster) : UseCase<String> {
-    override fun execute(): String =
-        manager().createDumpster(dumpster)
+class CreateDumpsterUseCase(private val dumpster: Dumpster) : UseCase<Dumpster> {
+    override fun execute(): Dumpster = manager().createDumpster(dumpster)
 }
--- src/main/kotlin/swc/usecases/OpenDumpsterUseCase.kt
+++ src/main/kotlin/swc/usecases/OpenDumpsterUseCase.kt
@@ -5,10 +5,11 @@ import swc.entities.Dumpster
 class OpenDumpsterUseCase(
     private val id: String,
     private val timeout: Long = Dumpster.TIMEOUT_MS,
-) : UseCase<Unit> {
-    override fun execute() {
+) : UseCase<Dumpster> {
+    override fun execute(): Dumpster {
         val dumpster = manager().getDumpsterById(id)
         if (dumpster.isAvailable()) manager().openDumpster(dumpster.id)
         manager().closeAfterTimeout(dumpster.id, timeout)
+        return manager().getDumpsterById(id)
     }
 }
