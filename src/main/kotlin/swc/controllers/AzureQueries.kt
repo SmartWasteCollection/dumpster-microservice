5557b8a6dd207adec440149f2958837d22ac9c9a
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-08 17:29:23 +0200

    feat: add routes for dumpster usecases

--- src/main/kotlin/swc/controllers/AzureQueries.kt
+++ src/main/kotlin/swc/controllers/AzureQueries.kt
@@ -4,7 +4,8 @@ object AzureQueries {
     private const val IS_DUMPSTER_DT_CONDITION = "WHERE IS_OF_MODEL('${AzureConstants.DUMPSTER_DT_MODEL_ID}')"
     private const val IS_CP_DT_CONDITION = "WHERE IS_OF_MODEL('${AzureConstants.COLLECTION_POINT_DT_MODEL_ID}')"
     const val GET_ALL_DT = "SELECT * FROM digitaltwins"
-    const val GET_COUNT_DT_QUERY = "SELECT COUNT() FROM digitaltwins"
+    const val GET_COUNT_DUMPSTER_QUERY = "SELECT COUNT() FROM digitaltwins $IS_DUMPSTER_DT_CONDITION"
+    const val GET_COUNT_CP_QUERY = "SELECT COUNT() FROM digitaltwins $IS_CP_DT_CONDITION"
     const val GET_ALL_DUMPSTERS_QUERY = "$GET_ALL_DT $IS_DUMPSTER_DT_CONDITION"
     const val GET_ALL_CP_QUERY = "$GET_ALL_DT $IS_CP_DT_CONDITION"
 }
--- src/main/kotlin/swc/controllers/DumpsterRestApi.kt
+++ src/main/kotlin/swc/controllers/DumpsterRestApi.kt
@@ -1,17 +1,27 @@
 package swc.controllers
 
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import swc.adapters.DumpsterDeserialization.parse
+import swc.adapters.DumpsterDeserialization.toDumpster
 import swc.entities.Dumpster
 import swc.usecases.dumpster.CloseDumpsterUseCase
+import swc.usecases.dumpster.CreateDumpsterUseCase
+import swc.usecases.dumpster.DeleteDumpsterUseCase
 import swc.usecases.dumpster.GetDumpsterByIdUseCase
 import swc.usecases.dumpster.GetDumpstersUseCase
+import swc.usecases.dumpster.OpenDumpsterUseCase
+import swc.usecases.dumpster.UpdateDumpsterVolumeUseCase
 
 @RestController
+@CrossOrigin()
 @RequestMapping("/dumpsters")
 class DumpsterRestApi {
 
@@ -21,11 +31,29 @@ class DumpsterRestApi {
     @GetMapping("/{id}")
     fun getDumpsterById(@PathVariable id: String): Dumpster = GetDumpsterByIdUseCase(id).execute()
 
+    @PostMapping("/")
+    fun createDumpster(@RequestBody body: String) = parse(body).apply {
+        val dumpster = this.getAsJsonObject("dumpster")
+        dumpster["id"] ?: dumpster.addProperty("id", AzureDTManager.calculateNextDumpsterId())
+    }.let {
+        CreateDumpsterUseCase(
+            it.getAsJsonObject("dumpster").toDumpster(),
+            it.getAsJsonPrimitive("collectionPointId").asString
+        ).execute()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteDumpster(@PathVariable id: String): Dumpster = DeleteDumpsterUseCase(id).execute()
+
+    @PutMapping("/open/{id}")
+    fun openDumpster(@PathVariable id: String): Dumpster = OpenDumpsterUseCase(id).execute()
+
     @PutMapping("/close/{id}")
     fun closeDumpster(@PathVariable id: String): Dumpster = CloseDumpsterUseCase(id).execute()
 
-    @PostMapping("/")
-    fun createDumpster(@RequestBody dumpster: Dumpster) {
-        println(dumpster)
-    }
+    @PutMapping("/volume/{id}")
+    fun updateVolume(@PathVariable id: String, @RequestBody body: String): Dumpster = UpdateDumpsterVolumeUseCase(
+        id,
+        parse(body).getAsJsonPrimitive("volume").asDouble,
+    ).execute()
 }
