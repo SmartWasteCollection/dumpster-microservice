ed8cfca717ca63d5b290c299b3f8bae8c478a013
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2022-07-08 14:33:47 +0200

    feat: add Spring framework to microservice

---
+++ src/main/kotlin/swc/DumpsterMicroservice.kt
@@ -0,0 +1,14 @@
+package swc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class DumpsterMicroservice
+
+fun main(args: Array<String>) {
+    runApplication<DumpsterMicroservice>(*args)
+
+    /*
+    */
+}
--- src/main/kotlin/swc/controllers/errors/CollectionPointNotFoundException.kt
+++ src/main/kotlin/swc/controllers/errors/CollectionPointNotFoundException.kt
@@ -1,3 +1,7 @@
 package swc.controllers.errors
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND, reason = "CollectionPoint not found")
 class CollectionPointNotFoundException(message: String) : Exception(message)
